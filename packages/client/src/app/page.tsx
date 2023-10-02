10a02d1ca7d0649022ad82108346d554490fc9a4
Aleksei Arro <polsck@gmail.com>
2023-10-02 18:43:19 +0400

    feat: add catalog link

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { APP_TITLE } from '@/shared/config';
 
 const RootPage: React.FC = () => {
--- packages/client/src/widgets/page-header/ui.tsx
+++ packages/client/src/widgets/page-header/ui.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { ShoppingCartIcon } from 'lucide-react';
 import { APP_TITLE } from '@/shared/config';
@@ -9,11 +10,14 @@ interface Props {
 
 export const PageHeader: React.FC<Props> = ({ className }) => {
   return (
-    <header className={cn('flex justify-between px-4 py-3', className)}>
-      <p>{APP_TITLE}</p>
-      <Link href="/cart" aria-label="Go to cart">
-        <ShoppingCartIcon aria-hidden />
-      </Link>
+    <header className={cn('flex items-center gap-x-8 px-4 py-3', className)}>
+      <Image src="/logo.svg" alt={APP_TITLE} width="37" height="32" priority />
+      <nav className="justify-content flex grow items-center gap-x-8">
+        <Link href="/">Catalog</Link>
+        <Link href="/cart" className="ml-auto" aria-label="Go to cart">
+          <ShoppingCartIcon aria-hidden />
+        </Link>
+      </nav>
     </header>
   );
 };
