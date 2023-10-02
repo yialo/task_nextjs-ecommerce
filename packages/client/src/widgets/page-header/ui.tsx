244beb24ea724c5040712039ad8e3520c6512eef
Aleksei Arro <polsck@gmail.com>
2023-10-03 00:43:01 +0400

    feat: stylize page header

--- packages/client/src/widgets/page-header/ui.tsx
+++ packages/client/src/widgets/page-header/ui.tsx
@@ -18,8 +18,14 @@ export const PageHeader: React.FC<Props> = ({ className }) => {
     >
       <Image src="/logo.svg" alt={APP_TITLE} width="37" height="32" priority />
       <nav className="justify-content flex grow items-center gap-x-8">
-        <Link href="/">Catalog</Link>
-        <Link href="/cart" className="ml-auto" aria-label="Go to cart">
+        <Link href="/" className="text-lg font-bold hover:underline">
+          Products
+        </Link>
+        <Link
+          href="/cart"
+          className="ml-auto hover:rotate-12"
+          aria-label="Go to cart"
+        >
           <ShoppingCartIcon aria-hidden />
         </Link>
       </nav>
