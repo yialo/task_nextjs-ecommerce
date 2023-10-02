29522fe062bd3f7b31473e3f6038ad09261dde26
Aleksei Arro <polsck@gmail.com>
2023-10-03 00:38:21 +0400

    feat: add gradient to page header

--- packages/client/src/widgets/page-header/ui.tsx
+++ packages/client/src/widgets/page-header/ui.tsx
@@ -10,7 +10,12 @@ interface Props {
 
 export const PageHeader: React.FC<Props> = ({ className }) => {
   return (
-    <header className={cn('flex items-center gap-x-8 px-4 py-3', className)}>
+    <header
+      className={cn(
+        'flex items-center gap-x-8 bg-[linear-gradient(90deg,_theme(colors.teal.100),_theme(colors.cyan.100))] px-4 py-3',
+        className,
+      )}
+    >
       <Image src="/logo.svg" alt={APP_TITLE} width="37" height="32" priority />
       <nav className="justify-content flex grow items-center gap-x-8">
         <Link href="/">Catalog</Link>
