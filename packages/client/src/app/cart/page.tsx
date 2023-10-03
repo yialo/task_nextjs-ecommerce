862b8187b5a818eb474376e6a6b6f2bcaa5985fa
Aleksei Arro <polsck@gmail.com>
2023-10-03 22:22:48 +0400

    feat: insert third-party html

--- packages/client/src/app/cart/page.tsx
+++ packages/client/src/app/cart/page.tsx
@@ -7,8 +7,9 @@ export const metadata: Metadata = {
 
 const CartPage: React.FC = () => {
   return (
-    <main className="page-center-container p-4">
+    <main className="page-center-container px-8 py-4">
       <h1>Cart</h1>
+      <button type="button">Place order</button>
     </main>
   );
 };
--- packages/client/src/app/product/[id]/page.tsx
+++ packages/client/src/app/product/[id]/page.tsx
@@ -1,4 +1,16 @@
+import Image from 'next/image';
+import * as React from 'react';
 import { readProductById } from '@/entities/product';
+
+const unescapeHtml = (escapedHtml: string) => {
+  return escapedHtml
+    .replaceAll('&lt;', '<')
+    .replaceAll('&gt;', '>')
+    .replaceAll('&quot;', '"')
+    .replaceAll('&amp;', '&')
+    .replaceAll('&nbsp;', '')
+    .replace(/ style=".*"/g, '');
+};
 
 interface Props {
   params: {
@@ -8,11 +20,19 @@ interface Props {
 
 const ProductPage: React.FC<Props> = async ({ params }) => {
   const product = await readProductById(params.id);
+  const { id, name, description, image, price } = product;
+  const unescapedDescription = unescapeHtml(description);
 
   return (
-    <main className="page-center-container">
-      <h1>Product: {params.id}</h1>
-      <pre>{JSON.stringify(product, null, 2)}</pre>
+    <main className="page-center-container px-8 py-4">
+      <h1>{name}</h1>
+      <Image src={image} alt={name} width={384} height={629} />
+      <div
+        className="third-party-html"
+        dangerouslySetInnerHTML={{ __html: unescapedDescription }}
+      />
+      <div>{price}</div>
+      <div>Add to cart</div>
     </main>
   );
 };
