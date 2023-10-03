5964537ea2d6a018ab03d80a119395955f3ae1d3
Aleksei Arro <polsck@gmail.com>
2023-10-04 00:35:05 +0400

    feat: improve ErrorBoundary styles

--- packages/client/src/app/error.tsx
+++ packages/client/src/app/error.tsx
@@ -7,17 +7,23 @@ interface Props {
   reset: () => void;
 }
 
-export default function Error({ error, reset }: Props) {
+const ErrorBoundary: React.FC<Props> = ({ error, reset }) => {
   React.useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
     <div className="page-center-container">
-      <p className="text-xl">Something went wrong!</p>
-      <button type="button" onClick={reset}>
+      <p className="mb-4 text-xl font-medium">Something went wrong!</p>
+      <button
+        className="rounded-lg bg-slate-900 px-4 py-3 text-white"
+        type="button"
+        onClick={reset}
+      >
         Try again
       </button>
     </div>
   );
-}
+};
+
+export default ErrorBoundary;
