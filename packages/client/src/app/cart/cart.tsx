a076f0287d030d6ea81e247d674d796b12a274dc
Aleksei Arro <polsck@gmail.com>
2023-10-04 03:38:56 +0400

    feat: adjust cart layout

--- packages/client/src/app/cart/cart.tsx
+++ packages/client/src/app/cart/cart.tsx
@@ -14,59 +14,57 @@ export const Cart: React.FC<Props> = ({ className }) => {
   const { productIds } = useCartModel();
   const productsQuery = useCartProductsQuery(productIds);
 
+  const fallbackClassName = cn(
+    'flex text-slate-600 justify-center items-center text-xl font-semibold',
+    className,
+  );
+
+  if (productsQuery.isInitail || productsQuery.isFetching) {
+    return <div className={fallbackClassName}>Loading...</div>;
+  }
+  if (productsQuery.error) {
+    return (
+      <div className={fallbackClassName}>
+        <div className="grid gap-4">
+          Something went wrong
+          <Button variant="negative" onClick={productsQuery.retry}>
+            Retry
+          </Button>
+        </div>
+      </div>
+    );
+  }
+  if (!productsQuery.data.length) {
+    return <div className={fallbackClassName}>Cart is empty</div>;
+  }
+
+  const orderTotal = productsQuery.data
+    .reduce((acc, product) => acc + product.priceInCents / 100, 0)
+    .toFixed(2);
+
   return (
-    <div className={cn('grid items-start', className)}>
-      {(() => {
-        const fallbackClassName =
-          'grid gap-4 text-slate-600 self-center justify-self-center text-xl font-semibold';
-
-        if (productsQuery.isInitail || productsQuery.isFetching) {
-          return <div className={fallbackClassName}>Loading...</div>;
-        }
-        if (productsQuery.error) {
+    <div className={cn('grid content-start', className)}>
+      <ul>
+        {productsQuery.data.map((product) => {
           return (
-            <div className={fallbackClassName}>
-              Something went wrong
-              <Button variant="negative" onClick={productsQuery.retry}>
-                Retry
-              </Button>
-            </div>
+            <li key={product.id}>
+              <div>{product.name}</div>
+              <div>{product.price}</div>
+            </li>
           );
-        }
-        if (!productsQuery.data.length) {
-          return <div className={fallbackClassName}>Cart is empty</div>;
-        }
-
-        const orderTotal = productsQuery.data
-          .reduce((acc, product) => acc + product.priceInCents / 100, 0)
-          .toFixed(2);
-
-        return (
-          <>
-            <ul>
-              {productsQuery.data.map((product) => {
-                return (
-                  <li key={product.id}>
-                    <div>{product.name}</div>
-                    <div>{product.price}</div>
-                  </li>
-                );
-              })}
-            </ul>
-            <div>
-              Total: <span className="font-semibold">{`$${orderTotal}`}</span>
-            </div>
-            <Button
-              className="md:w-1/2 md:justify-self-start"
-              onClick={() => {
-                console.log('Place order');
-              }}
-            >
-              Place order
-            </Button>
-          </>
-        );
-      })()}
+        })}
+      </ul>
+      <div>
+        Total: <span className="font-semibold">{`$${orderTotal}`}</span>
+      </div>
+      <Button
+        className="md:w-1/2 md:justify-self-start"
+        onClick={() => {
+          console.log('Place order');
+        }}
+      >
+        Place order
+      </Button>
     </div>
   );
 };
